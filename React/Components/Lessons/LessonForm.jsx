8597815f7f9a8fa9cd511dbc17d612b57a43de09
Enrique Lozano <97812994+SinonSnipe@users.noreply.github.com>
2022-08-17 19:08:31 -0700

    Add files via upload

---
+++ React/Components/Lessons/LessonForm.jsx
@@ -0,0 +1,192 @@
+import React, { useState, useEffect } from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import Dropzone from '../files/Dropzone';
+import lookup from '../../services/lookupService';
+import lessonSchema from '../../schema/lessonSchema';
+import PropTypes from 'prop-types';
+import toastr from '../../utils/toastr.js';
+import * as service from '../../services/lessonService';
+import './css/new-lesson.css';
+
+function LessonForm(props) {
+    const modalFormData = props.formData;
+    const [mappedDurations, setDurations] = useState([]);
+
+    const [newFormState, setNewFormState] = useState({
+        formData: {
+            id: 0,
+            title: '',
+            description: '',
+            durationTypeId: 0,
+            imageUrl: '',
+            fileUrl: '',
+            sortOrder: '',
+        },
+        isUpdateForm: false,
+    });
+
+    const setImgFormData = (fileData) => {
+        let imageFile = fileData[0].url;
+
+        setNewFormState((prevState) => {
+            const fs = { ...prevState };
+            fs.formData.imageUrl = imageFile;
+            return fs;
+        });
+    };
+
+    const getUploadImgReturn = (fileData) => {
+        setImgFormData(fileData);
+    };
+
+    const getUploadFileReturn = (fileData) => {
+        setFileFormData(fileData);
+    };
+
+    const setFileFormData = (fileData) => {
+        let newFile = fileData[0].url;
+
+        setNewFormState((prevState) => {
+            const fs = { ...prevState };
+            fs.formData.fileUrl = newFile;
+            return fs;
+        });
+    };
+
+    const onLookupSuccess = (response) => {
+        let mappedDurations = response.item.lessonDuration?.map(mapADuration);
+        setDurations(mappedDurations);
+    };
+
+    const mapADuration = (item, index) => (
+        <option value={item.id} key={`${item}_${index}`}>
+            {parseInt(item.name) / 60}
+            {' hours '}
+        </option>
+    );
+    const onLookupError = (err) => {
+        toastr.error(err);
+    };
+
+    useEffect(() => {
+        lookup(['LessonDuration']).then(onLookupSuccess).catch(onLookupError);
+        if (modalFormData) {
+            setNewFormState((prevState) => {
+                return {
+                    ...prevState,
+                    formData: {
+                        id: modalFormData.id,
+                        title: modalFormData.title,
+                        description: modalFormData.description,
+                        durationTypeId: modalFormData.type.id,
+                        imageUrl: modalFormData.imageUrl,
+                        fileUrl: modalFormData.fileUrl,
+                        sortOrder: modalFormData.sortOrder,
+                        durationName: modalFormData.type.name,
+                    },
+                    isUpdateForm: true,
+                };
+            });
+        }
+    }, []);
+
+    const handleSubmit = (values, { resetForm }) => {
+        parseInt(values.sortOrder);
+        values.type = {
+            id: values.durationTypeId,
+            name: values.durationName,
+        };
+
+        if (newFormState.isUpdateForm === false) {
+            resetForm();
+            service.create(values).then(onCreateSuccess).catch(onCreateError);
+        } else {
+            props.onHide();
+            props.onUpdateSubmit(values, props.lessonId);
+        }
+    };
+
+    const onCreateSuccess = (response) => {
+        toastr.success(`${response.title} added!`);
+        setNewFormState((prevState) => {
+            const fs = { ...prevState };
+            fs.formData.imageUrl = '';
+            fs.formData.fileUrl = '';
+            return fs;
+        });
+    };
+    const onCreateError = (err) => {
+        toastr.error(err);
+    };
+
+    return (
+        <React.Fragment>
+            <div className="file-upload-container">
+                <div className="dropzone-form">
+                    <div className="drop img-drop">
+                        <label className="upload-label">Upload Lesson Image</label>
+                        <Dropzone uploadedFiles={getUploadImgReturn} />
+                    </div>
+                    <div className="drop file-drop">
+                        <label className="upload-label">Upload Misc File</label>
+                        <Dropzone uploadedFiles={getUploadFileReturn} />
+                    </div>
+                </div>
+            </div>
+            <Formik
+                enableReinitialize={true}
+                initialValues={newFormState.formData}
+                onSubmit={handleSubmit}
+                validationSchema={lessonSchema}>
+                <Form>
+                    <div className="form-group form-new">
+                        <label htmlFor="title">Title</label>
+                        <Field type="text" name="title" className="form-control" />
+                        <ErrorMessage name="title" component="div" className="err-message" />
+                    </div>
+                    <div className="form-group form-new">
+                        <label htmlFor="sortOrder">Sort Order</label>
+                        <Field type="text" name="sortOrder" className="form-control"></Field>
+                        <ErrorMessage name="sortOrder" component="div" className="err-message" />
+                    </div>
+                    <div className="form-group form-new">
+                        <label htmlFor="description">Description</label>
+                        <Field component="textarea" name="description" className="form-control" />
+                        <ErrorMessage name="description" component="div" className="err-message" />
+                    </div>
+                    <div className="form-group form-new">
+                        <label htmlFor="durationTypeId">Time Duration</label>
+                        <Field as="select" name="durationTypeId" className="form-control">
+                            <option>Select a duration</option>
+                            {mappedDurations}
+                        </Field>
+                        <ErrorMessage name="durationTypeId" component="div" className="err-message" />
+                    </div>
+
+                    <button type="submit" className="btn btn-primary form-submit">
+                        Submit
+                    </button>
+                </Form>
+            </Formik>
+        </React.Fragment>
+    );
+}
+LessonForm.propTypes = {
+    formData: PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        title: PropTypes.string.isRequired,
+        description: PropTypes.string.isRequired,
+        type: PropTypes.shape({
+            id: PropTypes.number.isRequired,
+            name: PropTypes.string.isRequired,
+        }),
+        imageUrl: PropTypes.string,
+        fileUrl: PropTypes.string.isRequired,
+        sortOrder: PropTypes.number.isRequired,
+    }),
+    lessonId: PropTypes.number,
+    onUpdateSubmit: PropTypes.func,
+    onHide: PropTypes.func,
+};
+
+export default LessonForm;
---
+++ React/Components/Lessons/Lessons.jsx
@@ -0,0 +1,231 @@
+import React, { useState, useEffect, useCallback } from 'react';
+import { Link } from 'react-router-dom';
+import { Pagination } from 'react-bootstrap';
+import Lesson from './Lesson';
+import NavCard from '../host/NavCard';
+import PropTypes from 'prop-types';
+import toastr from '../../utils/toastr.js';
+import * as service from '../../services/lessonService';
+import * as names from './durationEnum';
+import './css/lessons.css';
+
+function Lessons(props) {
+    const currentUser = props.currentUser;
+    const [pageData, setPageData] = useState({
+        lessons: [],
+        lessonsComponents: [],
+        isShown: false,
+        current: 1,
+        index: 0,
+        size: 6,
+        items: [],
+        searchInput: '',
+    });
+    const [header, setHeader] = useState({
+        header: '',
+    });
+
+    useEffect(() => {
+        currentUser.roles.includes('User') &&
+            setHeader((prevState) => {
+                return { ...prevState, header: 'All Lessons' };
+            });
+
+        !currentUser.roles.includes('User') &&
+            setHeader((prevState) => {
+                return { ...prevState, header: 'My Lessons' };
+            });
+
+        currentUser.roles.includes('User') && get(pageData);
+
+        !currentUser.roles.includes('User') && getByCreatedBy(pageData);
+
+        pageData.searchInput.length > 0 && getBySearch(pageData.searchInput);
+    }, [pageData.current, pageData.searchInput, currentUser]);
+
+    const get = (currentPage) => {
+        service.get(currentPage.index, currentPage.size).then(onGetSuccess).catch(onGetError);
+    };
+
+    const getByCreatedBy = (currentPage) => {
+        service.getByCreatedBy(currentPage.index, currentPage.size).then(onGetSuccess).catch(onGetError);
+    };
+
+    const getBySearch = (searchString) => {
+        service.getBySearch(pageData.index, pageData.size, searchString).then(onGetSuccess).catch(onGetError);
+    };
+
+    const onGetSuccess = (response) => {
+        let pd = response.item;
+        let lessons = pd.pagedItems;
+
+        setPageData((prevState) => {
+            let newCurrent = [];
+            for (let number = 1; number <= pd.totalPages; number++) {
+                newCurrent.push(
+                    <Pagination.Item active={number === pageData.current} onClick={onPageClick} key={number}>
+                        {number}
+                    </Pagination.Item>
+                );
+            }
+            return { ...prevState, lessons: lessons, lessonsComponents: lessons.map(mapLesson), items: newCurrent };
+        });
+    };
+
+    const onInputChange = (e) => {
+        const target = e.target;
+        const searchInput = target.value;
+        setPageData((prevState) => ({ ...prevState, searchInput }));
+    };
+
+    const mapLesson = (lesson) => {
+        return (
+            <Lesson
+                currentUser={currentUser}
+                lesson={lesson}
+                key={lesson.id}
+                onDeleteClicked={onRemoveRequested}
+                onUpdateSubmit={onUpdateRequested}
+            />
+        );
+    };
+
+    const onPageClick = (e) => {
+        let newCurrent = parseInt(e.target.text);
+        setPageData((prevState) => {
+            return { ...prevState, index: newCurrent - 1, current: newCurrent };
+        });
+    };
+    const onPrevClick = () => {
+        setPageData((prevState) => {
+            let pd = { ...prevState };
+            pd.current = prevState.current - 1;
+            pd.index = prevState.index - 1;
+            return pd;
+        });
+    };
+    const onNextClick = () => {
+        setPageData((prevState) => {
+            let pd = { ...prevState };
+            pd.current = prevState.current + 1;
+            pd.index = prevState.index + 1;
+            return pd;
+        });
+    };
+
+    const onRemoveRequested = useCallback((currentLesson) => {
+        const handler = getSuccessHandler(currentLesson);
+        service.remove(currentLesson.id).then(handler).catch(onRemoveError);
+    }, []);
+
+    const getSuccessHandler = (currentLesson) => {
+        return () => {
+            setPageData((prevState) => {
+                const pd = { ...prevState };
+                pd.isCurrent = true;
+                const lessons = pd.lessons;
+                const idxOf = lessons.findIndex((lesson) => lesson.id === currentLesson.id);
+
+                if (idxOf >= 0) {
+                    lessons.splice(idxOf, 1);
+                    pd.lessonsComponents = lessons.map(mapLesson);
+                }
+                return pd;
+            });
+            toastr.success(`${currentLesson.title} has been deleted.`);
+        };
+    };
+
+    const onUpdateRequested = (values, lessonId) => {
+        service.update(values, lessonId).then(onUpdateSuccess).catch(onUpdateError);
+    };
+    const onUpdateSuccess = (response) => {
+        toastr.success('Update successful.');
+        setPageData((prevState) => {
+            const pd = { ...prevState };
+            const lessons = pd.lessons;
+            const idxOf = lessons.findIndex((lesson) => lesson.id === response.id);
+            lessons[idxOf] = response;
+            let nameValue = lessons[idxOf].durationTypeId;
+            lessons[idxOf].type.name = names.durationNames[nameValue];
+            pd.lessonsComponents = lessons.map(mapLesson);
+            return pd;
+        });
+    };
+
+    const onGetError = (err) => {
+        toastr.error(err);
+    };
+
+    const onUpdateError = (err) => {
+        toastr.error(err);
+    };
+
+    const onRemoveError = (err) => {
+        toastr.error(err);
+    };
+
+    return (
+        <React.Fragment>
+            <link
+                rel="stylesheet"
+                href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css"></link>
+            <div className="lessons-container">
+                {!currentUser.roles.includes('User') && <NavCard />}
+                <h4 className="lessons-header">{header.header}</h4>
+
+                {currentUser.roles.includes('User') && (
+                    <div className="search-container">
+                        <div className="input-group">
+                            <div className="form-outline search-outline">
+                                <input
+                                    placeholder="Search..."
+                                    type="search"
+                                    id="form1"
+                                    className="form-control search-bar"
+                                    onChange={onInputChange}
+                                    value={pageData.searchInput}
+                                />
+                            </div>
+                            <i className="bi bi-search"></i>
+                        </div>
+                    </div>
+                )}
+
+                {!currentUser.roles.includes('User') ? (
+                    <Link className="new-lesson" to="/lessons/new">
+                        <i className="bi bi-plus"></i>
+                        New
+                    </Link>
+                ) : (
+                    <>
+                        <button className="btn lessons-btn">
+                            <i className="bi bi-house-fill"></i>
+                            Saved Lessons
+                        </button>
+                    </>
+                )}
+
+                <div className="row page-data">
+                    {pageData.lessonsComponents}
+
+                    <Pagination>
+                        <Pagination.Prev onClick={onPrevClick} />
+                        {pageData.items}
+                        <Pagination.Next onClick={onNextClick} />
+                    </Pagination>
+                </div>
+            </div>
+        </React.Fragment>
+    );
+}
+Lessons.propTypes = {
+    currentUser: PropTypes.shape({
+        email: PropTypes.string.isRequired,
+        id: PropTypes.number.isRequired,
+        isLoggedIn: PropTypes.bool.isRequired,
+        roles: PropTypes.arrayOf(PropTypes.string).isRequired,
+        profilePic: PropTypes.string,
+    }),
+};
+export default Lessons;
