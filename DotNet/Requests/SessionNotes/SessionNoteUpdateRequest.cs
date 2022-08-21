2127d1fe467036a5eb79aadcf7df06e92fdc6a3e
Enrique Lozano <97812994+SinonSnipe@users.noreply.github.com>
2022-08-21 12:50:57 -0700

    Add files via upload

---
+++ DotNet/Requests/SessionNotes/SessionNoteUpdateRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sabio.Models.Requests.SessionNotes
+{
+    public class SessionNoteUpdateRequest : SessionNoteAddRequest, IModelIdentifier
+
+    {
+        public int Id { get; set; }
+    }
+}
