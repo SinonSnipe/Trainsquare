6ea8209b82aa98fa64ee975db89910b42117bdd8
Enrique Lozano <97812994+SinonSnipe@users.noreply.github.com>
2022-08-21 12:27:00 -0700

    Add files via upload

---
+++ DotNet/Requests/FAQs/FaqsSubmitRequest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sabio.Models.Requests.Faqs
+{
+    public class FaqsSubmitRequest
+    {
+        [Required]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "Please provide your name.")] 
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(250, ErrorMessage = "Please provide the reason for contacting us.")]
+        public string Question { get; set; }
+    }
+}
