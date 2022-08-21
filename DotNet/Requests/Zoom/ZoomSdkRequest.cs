129318e95aa9831a923aff5319e0620744bdf743
Enrique Lozano <97812994+SinonSnipe@users.noreply.github.com>
2022-08-21 12:55:31 -0700

    Add files via upload

---
+++ DotNet/Requests/Zoom/ZoomSdkRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sabio.Models.Requests.Zoom
+{
+    public class ZoomSdkRequest
+    {
+        public string Role { get; set; }
+        public string MeetingId { get; set; }
+    }
+}
