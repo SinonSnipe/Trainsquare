56515516862e8ce4f0236f37e0be60fea37cb0f7
Enrique Lozano <97812994+SinonSnipe@users.noreply.github.com>
2022-08-21 12:29:03 -0700

    Add files via upload

---
+++ DotNet/Requests/Files/FileTypeAddRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sabio.Models.Requests.Files
+{
+    public class FileTypeAddRequest
+    {
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
+        public string Name { get; set; }
+    }
+}
---
+++ DotNet/Requests/Files/FileUploadRequest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sabio.Models.Requests.Files
+{
+    public class FileUploadRequest
+    {
+        [Required]
+        [StringLength(255, MinimumLength = 3)]
+        public string Url { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
+        public string FileType { get; set; }
+        [Required]
+        [Range(1, int.MaxValue)]
+        public int CreatedBy { get; set; }
+    }
+}
