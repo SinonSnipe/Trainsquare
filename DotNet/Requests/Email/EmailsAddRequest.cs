d2bb42b6ef0938bfd611b738755289bc940bf1b7
Enrique Lozano <97812994+SinonSnipe@users.noreply.github.com>
2022-08-21 12:24:43 -0700

    Add files via upload

---
+++ DotNet/Requests/Email/EmailsAddRequest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sabio.Models.Requests.Email
+{
+    public class EmailsAddRequest
+    {
+        [Required]
+        public string HostEmail { get; set; }
+        [Required]
+        public string HostName { get; set; }
+        [Required]
+        public string ToEmails { get; set; }
+        [Required]
+        public string Subject { get; set; }
+        [Required]
+        public string Message { get; set; }
+        [Required]
+        public string MeetingUrl { get; set; }
+
+    }
+}
