6eaa385dc568efdedeec900cc14f056d0a0adae4
Enrique Lozano <97812994+SinonSnipe@users.noreply.github.com>
2022-08-21 12:51:20 -0700

    Add files via upload

---
+++ DotNet/Requests/Sessions/SessionUpdateRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sabio.Models.Requests.Sessions
+{
+    public class SessionUpdateRequest : SessionAddRequest, IModelIdentifier
+    {
+        public int Id { get; set; }
+
+    }
+}
