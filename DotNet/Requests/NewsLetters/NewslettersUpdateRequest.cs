a7ab14cf60bae5425e2a60dcb5c1c6fd4e7906f6
Enrique Lozano <97812994+SinonSnipe@users.noreply.github.com>
2022-08-21 12:38:54 -0700

    Add files via upload

---
+++ DotNet/Requests/NewsLetters/NewslettersUpdateRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sabio.Models.Requests.Newsletters
+{
+  
+    
+        public class NewslettersUpdateRequest : NewslettersAddRequest, IModelIdentifier
+        {
+            public int Id { get; set; }
+        }
+    
+}
