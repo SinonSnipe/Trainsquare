bf9203fa44b00fbf0420b90627eed6e98b2fcc2f
Enrique Lozano <97812994+SinonSnipe@users.noreply.github.com>
2022-08-21 12:52:36 -0700

    Add files via upload

---
+++ DotNet/Requests/Surveys/SurveyAnswerAddRequest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sabio.Models.Requests.Surveys
+{
+    public class SurveyAnswerAddRequest
+    {
+        [Required]
+        public int InstanceId { get; set; }
+
+        [Required]
+        public int QuestionId { get; set; }
+
+        public int AnswerOptionId { get; set; }
+
+        [StringLength(500, MinimumLength = 3)]
+        public string Answer { get; set; }
+
+        public int AnswerNumber { get; set; }
+    }
+}
---
+++ DotNet/Requests/Surveys/SurveyQuestionAnswerOptionAddRequest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sabio.Models.Requests.Surveys
+{
+    public class SurveyQuestionAnswerOptionAddRequest
+    {
+        [Required]
+        public int QuestionId { get; set; }
+
+        [StringLength(500, MinimumLength = 3)]
+        public string Text { get; set; }
+
+        [StringLength(100, MinimumLength = 1)]
+        public string Value { get; set; }
+
+        [StringLength(200, MinimumLength = 2)]
+        public string AdditionalInfo { get; set; }
+
+        [Required]
+        public int CreatedBy { get; set; }
+    }
+}
